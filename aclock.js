fbea6c21386eb6e7c053a0f4ec6bc27370866df3
rkumar <rajiv@manusis.com>
2013-05-20 16:06:35 +0530

    adding setinterval

--- aclock.js
+++ aclock.js
@@ -1,106 +1,101 @@
 console.log("in here");
 
-function clock()
-{
-    var now=new Date();
-    var c=document.getElementById("aCanvas").getContext('2d');
-        c.save();
-    c.clearRect(0,0,150,150);
-    c.translate(95,95);
-    c.scale(0.4,0.4);
-    c.rotate(-Math.PI/2);
-    c.strokeStyle="black";
-
-    c.lineWidth=8;
-    c.lineCap="round";
+function clock() {
+    var now = new Date();
+    var c = document.getElementById("aCanvas").getContext('2d');
     c.save();
-for(var i=0;i<12;i++)
-{
-    c.beginPath();
-    c.rotate(Math.PI/6);
-    c.moveTo(100,0);
-    c.lineTo(120,0);
-    c.stroke();
-}
-c.restore();
+    c.clearRect(0, 0, 150, 150);
+    c.translate(95, 95);
+    c.scale(0.4, 0.4);
+    c.rotate(-Math.PI / 2);
+    c.strokeStyle = "black";
 
-  c.save();
-    c.lineWidth=5;
-    for(var i=0;i<60;i++)
-    {
-        if(i%5!==0)
-        {
+    c.lineWidth = 8;
+    c.lineCap = "round";
+    c.save();
+    for (var i = 0; i < 12; i++) {
         c.beginPath();
-             c.moveTo(117,0);
-        c.lineTo(120,0);
+        c.rotate(Math.PI / 6);
+        c.moveTo(100, 0);
+        c.lineTo(120, 0);
         c.stroke();
-        }
-        c.rotate(Math.PI/30);
     }
     c.restore();
-var sec=now.getSeconds();
-var min=now.getMinutes();
-var hr=now.getHours();
-    hr=checkHr(hr);
-    c.fillStyle="black";
-c.save();
-c.rotate( hr*(Math.PI/6) + (Math.PI/360)*min + (Math.PI/21600)*sec) ;
-    c.strokeStyle="blue";
+
+    c.save();
+    c.lineWidth = 5;
+    for (var i = 0; i < 60; i++) {
+        if (i % 5 !== 0) {
+            c.beginPath();
+            c.moveTo(117, 0);
+            c.lineTo(120, 0);
+            c.stroke();
+        }
+        c.rotate(Math.PI / 30);
+    }
+    c.restore();
+    var sec = now.getSeconds();
+    var min = now.getMinutes();
+    var hr = now.getHours();
+    hr = checkHr(hr);
+    c.fillStyle = "black";
+    c.save();
+    c.rotate(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
+    c.strokeStyle = "blue";
     c.lineWidth = 14;
 
-c.beginPath();
-c.moveTo(-20,0);
-c.lineTo(80,0);
-c.stroke();
-c.restore();
-    c.save();
-    c.rotate((Math.PI/30)*min + (Math.PI/1800)*sec);
-    c.strokeStyle="blue";
-    c.lineWidth=10;
     c.beginPath();
-    c.moveTo(-28,0);
-    c.lineTo(112,0);
+    c.moveTo(-20, 0);
+    c.lineTo(80, 0);
     c.stroke();
     c.restore();
     c.save();
-    c.rotate((Math.PI/30)*sec);
-    c.strokeStyle="black";
-    c.filStyle="blue";
-    c.lineWidth=6;
+    c.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+    c.strokeStyle = "blue";
+    c.lineWidth = 10;
     c.beginPath();
-    c.moveTo(-30,0);
-    c.lineTo(83,0);
+    c.moveTo(-28, 0);
+    c.lineTo(112, 0);
+    c.stroke();
+    c.restore();
+    c.save();
+    c.rotate((Math.PI / 30) * sec);
+    c.strokeStyle = "black";
+    c.filStyle = "blue";
+    c.lineWidth = 6;
+    c.beginPath();
+    c.moveTo(-30, 0);
+    c.lineTo(83, 0);
     c.stroke();
 
     c.beginPath();
-    c.arc(0,0,10,0,Math.PI*2,true);
+    c.arc(0, 0, 10, 0, Math.PI * 2, true);
     c.fill();
 
     c.beginPath();
-    c.arc(95,0,10,0,Math.PI*2,true);
+    c.arc(95, 0, 10, 0, Math.PI * 2, true);
     c.stroke();
 
-    c.arc(0,0,3,0,Math.PI*2,true);
+    c.arc(0, 0, 3, 0, Math.PI * 2, true);
     c.fill();
 
-    c.fillStyle="blue";
+    c.fillStyle = "blue";
     c.fill();
     c.restore();
     c.beginPath();
-    c.lineWidth=14;
-    c.strokeStyle="black";
-    c.arc(0,0,142,0,Math.PI*2,true);
+    c.lineWidth = 14;
+    c.strokeStyle = "black";
+    c.arc(0, 0, 142, 0, Math.PI * 2, true);
     c.stroke();
     c.restore();
 }
-clock();
-function checkHr(h)
-{
+//clock();
+setInterval(clock, 1000);
+function checkHr(h) {
 
-    if(h>12)
-    {
-                h=h-"12";
+    if (h > 12) {
+        h = h - "12";
 
     }
-return h;
+    return h;
 }
--- dclock.js
+++ dclock.js
@@ -6,40 +6,36 @@
  * To change this template use File | Settings | File Templates.
  */
 console.log("in here");
-function clock()
-{
-
-   console.log("enter here");
-    var currentTime=new Date();
-    var d=currentTime.getDate();
-    var m=currentTime.getMonth();
-var y=currentTime.getYear();
-    var currentHour=currentTime.getHours();
-    var currentMin=currentTime.getMinutes();
-    var currentSec=currentTime.getSeconds();
-       currentMin=checkTime(currentMin);
-    currentSec=checkTime(currentSec);
-   currentHour=checkHour(currentHour);
-    var dis="date:" + d + "-" +m +"-" + y + "</br>" +"time:" +currentHour + ":" + currentMin + ":" + currentSec;
-    document.getElementById("p1").innerHTML=dis;
+function clock() {
+    console.log("enter here");
+    var currentTime = new Date();
+    var d = currentTime.getDate();
+    var m = currentTime.getMonth();
+    var y = currentTime.getYear();
+    var currentHour = currentTime.getHours();
+    var currentMin = currentTime.getMinutes();
+    var currentSec = currentTime.getSeconds();
+    currentMin = checkTime(currentMin);
+    currentSec = checkTime(currentSec);
+    currentHour = checkHour(currentHour);
+    var dis = "date:" + d + "-" + m + "-" + y + "</br>" + "time:" + currentHour + ":" + currentMin + ":" + currentSec;
+    document.getElementById("p1").innerHTML = dis;
 };
-   clock();
-function checkTime(i)
-{
-    if(i<10)
-    {
-        i="0" +i;
+//clock();
+setInterval(clock, 1000);
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
     }
     return i;
 }
-   function checkHour(h)
-   {
-       if(h>12)
-       {
-           h=h-"12";
-       }
-       return h;
-   }
+function checkHour(h) {
+    if (h > 12) {
+        h = h - "12";
+    }
+    return h;
+}
 
 
 
